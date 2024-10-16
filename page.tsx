6148ff83d67f052e695e9cb781480ed43af901c0
Muhammad Usman Asif <164504890+Usman-Philco123@users.noreply.github.com>
2024-10-16 21:47:19 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,11 @@
+
+
+import React from 'react'
+
+export default function NestedPage() {
+  return (
+    <div className='bg-yellow-400 text-red-600'>
+      <h1>This is my Nested Page of Services</h1>
+    </div>
+  )
+}
